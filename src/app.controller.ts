980d6bcfb6702a2bb75d8988a5bd976fa049865f
Likaterina <linainadelira@gmail.com>
2020-01-21 17:33:25 +0200

    validation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,30 @@
-import { Controller, Get, UseGuards, Post, Request, Body } from "@nestjs/common"
+import {
+  Controller,
+  Post,
+  Body,
+  UsePipes,
+  ValidationPipe
+} from "@nestjs/common"
 import { AuthService } from "../auth/auth.service"
 import { CreateUserDto } from "users/users.service"
 
+import { ApiResponse } from "@nestjs/swagger"
 
 @Controller("auth")
 export class AppController {
   constructor(private readonly authService: AuthService) {}
 
   @Post("registration")
-  async register(@Body() createUserDto: CreateUserDto): Promise<any>  {
-    await this.authService.register(createUserDto)
+  @ApiResponse({ status: 201, description: "Ok" })
+  @ApiResponse({ status: 401, description: "Unauthorized" })
+  register(@Body() createUserDto: CreateUserDto): Promise<CreateUserDto> {
+    return this.authService.register(createUserDto)
   }
 
   @Post("login")
-  async login(@Body() createUserDto: CreateUserDto): Promise<any>  {
-    await this.authService.login(createUserDto)
+  @ApiResponse({ status: 201, description: "Ok" })
+  @ApiResponse({ status: 403, description: "This user as already exists" })
+  login(@Body() createUserDto: CreateUserDto): Promise<CreateUserDto> {
+    return this.authService.login(createUserDto)
   }
-
 }
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { NestFactory } from "@nestjs/core"
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger"
 import { AppModule } from "./app.module"
 import { AuthModule } from 'auth/auth.module'
+import { ValidationPipe } from "@nestjs/common"
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule)
-
+  app.useGlobalPipes(new ValidationPipe());
   const options = new DocumentBuilder()
     .setTitle("auth")
     .setDescription("auth with jwt")
